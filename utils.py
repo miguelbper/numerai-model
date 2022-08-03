416ebc67a0e1120689b6d7c1c932b4af88ebbcc6
Miguel <mbpereira94@gmail.com>
2022-08-04 00:23:14 +0100

    MultiTargetTrainer class.
    Former-commit-id: 4d6d7348f8ed40400346b7b6fc40f962eaf25722

--- utils.py
+++ utils.py
@@ -2,8 +2,9 @@ import json
 import joblib
 import numpy as np
 from sklearn.base import BaseEstimator, RegressorMixin
-from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.multioutput import MultiOutputRegressor
 from sklearn.model_selection._split import _BaseKFold, indexable, _num_samples
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from copy import deepcopy
 from math import ceil
 
@@ -100,6 +101,26 @@ class EraSubsampler(BaseEstimator, RegressorMixin):
             y_pred += self.model[i].predict(X)
         y_pred /= k
         return y_pred
+
+
+class MultiTargetTrainer(BaseEstimator, RegressorMixin):
+    def __init__(self, estimator, features=None, n_jobs=None):
+        self.estimator = estimator
+        self.features = features
+        self.n_jobs = n_jobs
+
+    def fit(self, X, y, **fit_params):
+        self.model = MultiOutputRegressor(self.estimator, n_jobs=self.n_jobs)
+        self.model.fit(X, y, **fit_params)
+        self.is_fitted_ = True
+        return self
+
+    def predict(self, X):
+        check_is_fitted(self, 'is_fitted_')
+        y_preds = self.model.predict(X)
+        indices = self.features
+        indices = indices if indices is not None else range(len(y_preds[0]))
+        return np.average(y_preds[:, indices], axis=1)
 
 
 # The following class is taken from the examples from Numerai
