5251ae2e8df85ed796718fb7587b2d4f6b620240
Miguel <mbpereira94@gmail.com>
2022-08-05 14:14:02 +0100

    class FeatureNeutralizer.
    Former-commit-id: 1ba56036703e4de110885f95042ba818dfc2d0b4

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.multioutput import MultiOutputRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 from sklearn.model_selection._split import _BaseKFold, indexable, _num_samples
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from copy import deepcopy
@@ -61,23 +63,20 @@ del f
 # ---------------
 # score functions
 
-def corr(a, b, rank_a=False, rank_b=False):
-    def maybe_rank(a, rank):
-        if isinstance(rank, pd.Series):
-            return a.groupby(rank).apply(lambda x: x.rank(pct=True))
-        elif rank:
-            return a.rank(pct=True)
-        else:
-            return a
+def np_(df):
+    return df if isinstance(df, np.ndarray) else df.to_numpy()
 
-    def to_np(df):
-        if isinstance(df, np.ndarray):
-            return df
-        else:
-            return df.to_numpy()
-    
-    a = to_np(maybe_rank(a, rank_a))
-    b = to_np(maybe_rank(b, rank_b))
+def maybe_rank(a, rank):
+    if isinstance(rank, np.ndarray) or isinstance(rank, pd.Series):
+        return a.groupby(np_(rank)).apply(lambda x: x.rank(pct=True))
+    elif rank:
+        return a.rank(pct=True)
+    else:
+        return a
+
+def corr(a, b, rank_a=False, rank_b=False):
+    a = np_(maybe_rank(pd.DataFrame(a), rank_a))
+    b = np_(maybe_rank(pd.DataFrame(b), rank_b))
     n = 1 if a.ndim == 1 else len(a[0])
     return np.corrcoef(a, b, rowvar=False)[0:n, n:].squeeze()
 
@@ -146,9 +145,9 @@ class EraSubsampler(BaseEstimator, RegressorMixin):
 
 
 class MultiTargetTrainer(BaseEstimator, RegressorMixin):
-    def __init__(self, estimator, features=None, n_jobs=None):
+    def __init__(self, estimator, targets=None, n_jobs=None):
         self.estimator = estimator
-        self.features = features
+        self.targets = targets
         self.n_jobs = n_jobs
 
     def fit(self, X, y, **fit_params):
@@ -160,9 +159,81 @@ class MultiTargetTrainer(BaseEstimator, RegressorMixin):
     def predict(self, X):
         check_is_fitted(self, 'is_fitted_')
         y_preds = self.model.predict(X)
-        indices = self.features
+        indices = self.targets
         indices = indices if indices is not None else range(len(y_preds[0]))
         return np.average(y_preds[:, indices], axis=1)
+
+
+# alternative way of implementing FeatureNeutralizer:
+# instead of giving groups as separate argument, give them inside matrix X
+# adv: more compatible with sklearn
+# disadv: can't use GridSearchCV anyway (unpractical), bandaid, 
+# less clarity when using class
+class FeatureNeutralizer(BaseEstimator):
+    def __init__(self, estimator, n_features, alpha):
+        self.estimator = estimator
+        self.n_features = n_features
+        self.alpha = alpha
+
+    def fit(self, X, y, **fit_params):
+        # X, y = check_X_y(X, y, accept_sparse=True)
+        self.estimator.fit(X, y, **fit_params)
+        self.is_fitted_ = True
+        return self
+    
+    def compute_y_pred(self, X):
+        # checks
+        X = check_array(X, accept_sparse=True)
+        check_is_fitted(self, 'is_fitted_')
+        # computations
+        self.y_pred = self.estimator.predict(X)
+
+    def compute_y_linr(self, X, groups):
+        # checks
+        X = check_array(X, accept_sparse=True)
+        check_is_fitted(self, 'is_fitted_')
+        if not hasattr(self, 'y_pred'):
+            self.compute_y_pred(X)
+        if self.n_features == 0:
+            self.y_linr = 0
+            return
+        # computations
+        y_pred = self.y_pred
+        groups = np_(groups)
+        # n riskiest features
+        exposures = corr(X, y_pred, rank_a=groups, rank_b=groups)
+        riskiest = [(v, i) for i, v in enumerate(exposures)]
+        riskiest = sorted(riskiest, reverse=True)
+        riskiest = riskiest[0:self.n_features]
+        riskiest = [i for _, i in riskiest]
+        # auxiliary function
+        def aux_linreg(df):
+            X_ = df[df.columns[0:-1]]
+            y_ = df[df.columns[-1]]
+            model = LinearRegression()
+            model.fit(X_, y_)
+            return pd.Series(model.predict(X_) - model.intercept_)
+        # result
+        R = X[:, riskiest]
+        df_Ry = pd.DataFrame(np.hstack((R, np.atleast_2d(y_pred).T)))
+        self.y_linr = df_Ry.groupby(groups).apply(aux_linreg).to_numpy()
+
+    def predict(self, X, groups):
+        # checks
+        X = check_array(X, accept_sparse=True)
+        check_is_fitted(self, 'is_fitted_')
+        if not hasattr(self, 'y_pred'):
+            self.compute_y_pred(X)
+        if not hasattr(self, 'y_linr'):
+            self.compute_y_linr(X, groups)
+        # computations
+        return self.y_pred - self.alpha * self.y_linr
+
+    # cannot import score from regressor mixin
+    # because predict needs a groups parameter
+    def score(self, X, y, groups, sample_weight=None):
+        y_pred = self.predict(X, groups)
+        return r2_score(y, y_pred, sample_weight=sample_weight)
 
 
 # The following class is taken from the examples from Numerai
@@ -179,8 +250,8 @@ class TimeSeriesSplitGroups(_BaseKFold):
         n_groups = len(group_list)
         if n_folds > n_groups:
             raise ValueError(
-                ("Cannot have number of folds ={0} greater"
-                 " than the number of samples: {1}.").format(n_folds, n_groups))
+                ("Cannot have n_folds ={0} greater"
+                 " than the n_samples: {1}.").format(n_folds, n_groups))
         indices = np.arange(n_samples)
         test_size = (n_groups // n_folds)
         test_starts = range(test_size + n_groups % n_folds,
@@ -188,4 +259,5 @@ class TimeSeriesSplitGroups(_BaseKFold):
         test_starts = list(test_starts)[::-1]
         for test_start in test_starts:
             yield (indices[groups.isin(group_list[:test_start])],
-                   indices[groups.isin(group_list[test_start:test_start + test_size])])
+                   indices[groups.isin(group_list[test_start
+                                                  :test_start + test_size])])
