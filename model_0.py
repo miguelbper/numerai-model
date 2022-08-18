49c38f93cbe3d7c6f46ad303beb6e0c188476063
Miguel <mbpereira94@gmail.com>
2022-08-18 23:14:36 +0100

    CV for model_0.

--- model_0.py
+++ model_0.py
@@ -55,276 +55,87 @@ spl = TimeSeriesSplitGroups(n_splits=n_splits)
 # number of era subsamples
 # ----------------------------------------------------------------------
 
-# subsamples = range(1, 13)
-# cv_era = np.zeros((len(subsamples), n_splits))
+ran_ns = range(1, 13)
 
-# for i in tqdm_(subsamples, desc='CV number of era subsamples'):
-#     for j, (trn, val) in tqdm_(enumerate(spl.split(X, y, e)), 
-#                           desc='CV fold', 
-#                           total=n_splits):
-#         X_trn = X.iloc[trn]
-#         X_val = X.iloc[val]
-#         y_trn = y.iloc[trn]
-#         y_val = y.iloc[val]
-#         e_trn = e.iloc[trn]
-#         e_val = e.iloc[val]
+cv_era = {'n_subsamples': []}
+for j in range(n_splits):
+    cv_era[f'split_{j + 1}'] = []
 
-#         model_name = f'model-0/saved-variables/cv_era_{i}_{j}.pkl'
-#         try:
-#             model = joblib.load(model_name)
-#         except:
-#             model = EraSubsampler(LGBMRegressor(**params), n_subsamples=i)
-#             model.fit(X_trn, y_trn, eras=e_trn)
-#             joblib.dump(model, model_name)
+for i in tqdm(ran_ns, desc='n_subsamples', leave=None, position=0):
+    cv_era['n_subsamples'].append(i)
 
-#         y_name = f'model-0/saved-variables/y_era_{i}_{j}.pkl'
-#         try:
-#             y_val_pred = joblib.load(y_name)
-#         except:
-#             y_val_pred = model.predict(X_val)
-#             joblib.dump(y_val_pred, y_name)
+    for j, (trn, val) in tqdm(enumerate(spl.split(X, y, e)), 
+                                        desc='CV fold',
+                                        leave=None,
+                                        position=1,
+                                        total=n_splits):
+        X_trn = X.iloc[trn]
+        X_val = X.iloc[val]
+        y_trn = y.iloc[trn]
+        y_val = y.iloc[val]
+        e_trn = e.iloc[trn]
+        e_val = e.iloc[val]
 
-#         cv_era[i-1, j] = corr(y_val, y_val_pred, rank_b=e_val)
+        model_name = f'model-0/saved-variables/cv_era_{i}_{j}.pkl'
+        try:
+            model = joblib.load(model_name)
+        except:
+            model = EraSubsampler(LGBMRegressor(**params), n_subsamples=i)
+            model.fit(X_trn, y_trn, eras=e_trn)
+            joblib.dump(model, model_name)
 
-# cv_era = pd.DataFrame(cv_era)
-# cv_era['mean'] = cv_era.mean(axis=1)
-# cv_era.to_excel('model-0/cross-validation/cv_era.xlsx')
+        y_name = f'model-0/saved-variables/y_era_{i}_{j}.pkl'
+        try:
+            y_val_pred = joblib.load(y_name)
+        except:
+            y_val_pred = model.predict(X_val)
+            joblib.dump(y_val_pred, y_name)
+
+        c = corr(y_val, y_val_pred, rank_b=e_val)
+        cv_era[f'split_{j + 1}'].append(c)
+
+cv_era = pd.DataFrame(cv_era)
+cv_era['mean'] = cv_era[[f'split_{j+1}' for j in range(n_splits)]].mean(axis=1)
+cv_era.to_excel('model-0/cross-validation/cv_era.xlsx')
+n_subsamples = cv_era['n_subsamples'][cv_era['mean'].argmax()] 
+# n_subsamples = 7
 
 
 # ----------------------------------------------------------------------
-# weights for training on many targets + feature neutralization
+# weights for training on many targets
 # ----------------------------------------------------------------------
 
-# n_targ = 10
-# n_feat = list(range(1, 16))
-# alphas = np.arange(0, 1, 0.01)
-
-# y = Y[Y_COLS[:n_targ]]
-
-# cv_tar_wei = np.zeros((n_targ, n_splits))
-# cv_tar_cor = np.zeros((n_splits))
-
-# cv_neu_alp = np.zeros((len(n_feat), n_splits))
-# cv_neu_cor = np.zeros((len(n_feat), n_splits))
-# cv_neu_opt = np.zeros((3, n_splits))
-
-# for j, (trn, val) in tqdm_(enumerate(spl.split(X, y, e)), 
-#                            desc='CV targets', 
-#                            total=n_splits):
-#     X_trn = X.iloc[trn]
-#     X_val = X.iloc[val]
-#     y_trn = y.iloc[trn]
-#     y_val = y.iloc[val]
-#     e_trn = e.iloc[trn]
-#     e_val = e.iloc[val]
-
-#     model_name = f'model-0/saved-variables/cv_targets_{j}.pkl'
-#     try:
-#         model = joblib.load(model_name)
-#     except IOError:
-#         model = LGBMRegressor(**params)
-#         model = EraSubsampler(model, n_subsamples=7)
-#         model = MultiOutputRegressor(model)
-#         model.fit(X_trn, y_trn, eras=e_trn)
-#         joblib.dump(model, model_name)
-
-#     y_val_true = y_val[Y_TRUE]
-#     y_name = f'model-0/saved-variables/y_targets_{j}.pkl'
-#     try:
-#         y_val_pred = joblib.load(y_name)
-#     except:
-#         y_val_pred = model.predict(X_val)
-#         joblib.dump(y_val_pred, y_name)
-
-#     def f_corr(w):
-#         return corr(y_val_true, y_val_pred @ w, rank_b=e_val)
-
-#     w_max = maximum(f_corr, n_targ)
-#     y_val_pred_w = y_val_pred @ w_max
-#     cv_tar_wei[:, j] = w_max
-#     cv_tar_cor[j] = f_corr(w_max)
-
-#     # CV for feature neutralization
-#     exposures = corr(X_val, y_val_pred_w, rank_a=e_val, rank_b=e_val)
-#     riskiest = [(v, i) for i, v in enumerate(exposures)]
-#     riskiest = sorted(riskiest, reverse=True)
-#     riskiest = [i for _, i in riskiest]
-
-#     for k, n_features in enumerate(n_feat):
-#         riskiest_n = riskiest[0:n_features]
-#         riskiest_n = [x_cols[i] for i in riskiest_n]
-
-#         def aux_linreg(df):
-#             X_ = df[df.columns[0:-1]]
-#             y_ = df[df.columns[-1]]
-#             model = LinearRegression()
-#             model.fit(X_, y_)
-#             return pd.Series(model.predict(X_) - model.intercept_)
-
-#         R = X_val[riskiest_n].to_numpy()
-#         df_Ry = pd.DataFrame(np.hstack((R, np.atleast_2d(y_val_pred_w).T)))
-#         y_linr = df_Ry.groupby(e_val.to_numpy()).apply(aux_linreg).to_numpy()
-
-#         cs = np.array([
-#             corr(y_val_true, y_val_pred_w - a * y_linr, rank_b=e_val) 
-#             for a in alphas
-#         ])
-#         mx = np.argmax(cs)
-#         cv_neu_alp[k, j] = alphas[mx]
-#         cv_neu_cor[k, j] = cs[mx]
-
-#         fig, ax = plt.subplots()
-#         ax.plot(alphas, cs)
-#         ax.set_title(f'split = {j}, n_features = {n_features}')
-#         fig.savefig(f'model-0/figures/corr_alpha_{j}_{n_features}.png')
-#         plt.close(fig)
-        
-#     mx = np.argmax(cv_neu_cor, axis=0)[j]
-#     cv_neu_opt[0, j] = n_feat[mx]
-#     cv_neu_opt[1, j] = cv_neu_alp[mx, j]
-#     cv_neu_opt[2, j] = cv_neu_cor[mx, j]
-
-
-# cv_tar_wei = pd.DataFrame(cv_tar_wei)
-# cv_tar_wei['mean'] = cv_tar_wei.mean(axis=1)
-# cv_tar_wei.to_excel('model-0/cross-validation/cv_tar_wei.xlsx')
-
-# cv_tar_cor = pd.DataFrame(cv_tar_cor)
-# cv_tar_cor.to_excel('model-0/cross-validation/cv_tar_cor.xlsx')
-
-# cv_neu_alp = pd.DataFrame(cv_neu_alp)
-# cv_neu_alp.to_excel('model-0/cross-validation/cv_neu_alp.xlsx')
-
-# cv_neu_cor = pd.DataFrame(cv_neu_cor)
-# cv_neu_cor.to_excel('model-0/cross-validation/cv_neu_cor.xlsx')
-
-# cv_neu_opt = pd.DataFrame(cv_neu_opt)
-# cv_neu_opt.to_excel('model-0/cross-validation/cv_neu_opt.xlsx')
-
-# ----------------------------------------------------------------------
-# conclusion
-# ----------------------------------------------------------------------
-
-# era subsamples: 7
-
+# choose the best weights for each split and take the average (using a 
+# discrete gradient descent algorithm):
 # targets: 
-# 0: 0,5525      ->  0.55 
-# 1: 0,1225      ->  0.12
-# 2: -0,2475     -> -0.25
-# 3: 1,38778E-17 ->
-# 4: 0,1725      ->  0.17
-# 5: 0,0375      ->
-# 6: -0,03       ->
-# 7: -0,05       ->
-# 8: 0,3675      ->  0.37
-# 9: 0,075       ->
-# should retrain targets with only 0, 1, 2, 4, 8 available
-# also with only 0, 1, 4, 8 (since 2 gives negative coef)
+# 0:  0,5525 
+# 1:  0,1225
+# 2: -0,2475
+# 3:  0,0000
+# 4:  0,1725
+# 5:  0,0375
+# 6: -0,0300
+# 7: -0,0500 
+# 8:  0,3675
+# 9:  0,0750
+# consider only targets 0, 1, 2, 4, 8 and choose the weights which give
+# the best corr (on average, over the splits)
 
-# feature neutralization:
-# n_features: around 10 maybe
-# alpha: around 10% - 40%
+ran_w1 = [x / 100 for x in range(0, 20, 5)]
+ran_w2 = [x / 100 for x in range(-30, 5, 5)]
+ran_w4 = [x / 100 for x in range(0, 30, 5)]
+ran_w8 = [x / 100 for x in range(0, 45, 5)]
+len_ws = len(ran_w1) * len(ran_w2) * len(ran_w4) * len(ran_w8)
 
-
-# ----------------------------------------------------------------------
-# retrain / grid search
-# ----------------------------------------------------------------------
-
-# target weights
-y = Y
-
-# ran_w1 = [x / 100 for x in range(0, 25, 5)] # np.arange(0.1, 0.2, 0.01)
-# ran_w2 = [x / 100 for x in range(-35, 5, 5)] # np.arange(0, -0.3, -0.05)
-# ran_w4 = [x / 100 for x in range(0, 25, 5)] # np.arange(0.1, 0.2, 0.01)
-# ran_w8 = [x / 100 for x in range(20, 45, 5)] # np.arange(0.3, 0.4, 0.01)
-# len_ws = len(ran_w1) * len(ran_w2) * len(ran_w4) * len(ran_w8)
-
-# corr_dict = {
-#     'w0': [],
-#     'w1': [],
-#     'w2': [],
-#     'w4': [],
-#     'w8': [],
-#     'c0': [],
-#     'c1': [],
-#     'c2': [],
-#     'c3': [],
-# }
-
-# for j, (trn, val) in tqdm(enumerate(spl.split(X, y, e)), 
-#                            desc='cv split', 
-#                            total=n_splits,
-#                            leave=None,
-#                            position=0):
-#     X_trn = X.iloc[trn]
-#     X_val = X.iloc[val]
-#     y_trn = y.iloc[trn]
-#     y_val = y.iloc[val]
-#     e_trn = e.iloc[trn]
-#     e_val = e.iloc[val]
-
-#     model_name = f'model-0/saved-variables/cv_targets_{j}.pkl'
-#     try:
-#         model = joblib.load(model_name)
-#     except IOError:
-#         model = LGBMRegressor(**params)
-#         model = EraSubsampler(model, n_subsamples=7)
-#         model = MultiOutputRegressor(model)
-#         model.fit(X_trn, y_trn, eras=e_trn)
-#         joblib.dump(model, model_name)
-
-#     y_val_true = y_val[Y_TRUE]
-#     y_name = f'model-0/saved-variables/y_targets_{j}.pkl'
-#     try:
-#         y_val_pred = joblib.load(y_name)
-#     except:
-#         y_val_pred = model.predict(X_val)
-#         joblib.dump(y_val_pred, y_name)
-
-#     # ran_w1 = [x / 100 for x in range(10, 20)]
-#     # ran_w2 = [x / 100 for x in range(0, -30, -5)]
-#     # ran_w4 = [x / 100 for x in range(10, 20)]
-#     # ran_w8 = [x / 100 for x in range(30, 40)]
-#     # ran_ws = product(ran_w1, ran_w2, ran_w4, ran_w8)
-#     # len_ws = len(ran_w1) * len(ran_w2) * len(ran_w4) * len(ran_w8)
-
-#     for (w1, w2, w4, w8) in tqdm(product(ran_w1, ran_w2, ran_w4, ran_w8), 
-#                                  desc='weights', 
-#                                  total=len_ws,
-#                                  leave=False,
-#                                  position=1):
-#         w0 = 1 - w1 - w2 - w4 - w8
-#         w = np.array([w0, w1, w2, 0, w4, 0, 0, 0, w8, 0])
-#         y_val_pred_w = y_val_pred @ w
-        
-#         cr = corr(y_val_true, y_val_pred_w, rank_b=e_val)
-
-#         corr_dict[f'c{j}'].append(cr)
-#         if j == 0:
-#             corr_dict['w0'].append(w0)
-#             corr_dict['w1'].append(w1)
-#             corr_dict['w2'].append(w2)
-#             corr_dict['w4'].append(w4)
-#             corr_dict['w8'].append(w8)
-
-# corr_df = pd.DataFrame(corr_dict)
-# corr_df['ca'] = corr_df[['c0', 'c1', 'c2', 'c3']].mean(axis=1)
-# corr_df = corr_df.sort_values('ca', ascending=False)
-# corr_df.to_excel('model-0/cross-validation/cv_era_grd.xlsx')
-
-# feature neutralization
-
-ran_nf = np.arange(5, 55, 5)
-ran_al = np.arange(0, 1, 0.05)
-
-corr_dict = {
-    'nf': [],
-    'al': [],
-    'c0': [],
-    'c1': [],
-    'c2': [],
-    'c3': [],
+cv_tar = {
+    'w[0]': [],
+    'w[1]': [],
+    'w[2]': [],
+    'w[4]': [],
+    'w[8]': [],
 }
+for j in range(n_splits):
+    cv_tar[f'split_{j + 1}'] = []
 
 for j, (trn, val) in tqdm(enumerate(spl.split(X, y, e)), 
                            desc='cv split', 
@@ -333,8 +144,8 @@ for j, (trn, val) in tqdm(enumerate(spl.split(X, y, e)),
                            position=0):
     X_trn = X.iloc[trn]
     X_val = X.iloc[val]
-    y_trn = y.iloc[trn]
-    y_val = y.iloc[val]
+    y_trn = Y.iloc[trn]
+    y_val = Y.iloc[val]
     e_trn = e.iloc[trn]
     e_val = e.iloc[val]
 
@@ -343,21 +154,76 @@ for j, (trn, val) in tqdm(enumerate(spl.split(X, y, e)),
         model = joblib.load(model_name)
     except IOError:
         model = LGBMRegressor(**params)
-        model = EraSubsampler(model, n_subsamples=7)
+        model = EraSubsampler(model, n_subsamples=n_subsamples)
         model = MultiOutputRegressor(model)
         model.fit(X_trn, y_trn, eras=e_trn)
         joblib.dump(model, model_name)
 
-    y_val_true = y_val[Y_TRUE]
     y_name = f'model-0/saved-variables/y_targets_{j}.pkl'
     try:
         y_val_pred = joblib.load(y_name)
     except:
         y_val_pred = model.predict(X_val)
         joblib.dump(y_val_pred, y_name)
+    y_val_true = y_val[Y_TRUE]
 
-    
-    w = np.array([0.6, 0.1, -0.25, 0, 0.2, 0, 0, 0, 0.35, 0])
+    for (w1, w2, w4, w8) in tqdm(product(ran_w1, ran_w2, ran_w4, ran_w8), 
+                                 desc='weights', 
+                                 total=len_ws,
+                                 leave=False,
+                                 position=1):
+        w0 = 1 - w1 - w2 - w4 - w8
+        w = np.array([w0, w1, w2, 0, w4, 0, 0, 0, w8, 0])
+        y_val_pred_w = y_val_pred @ w
+        
+        c = corr(y_val_true, y_val_pred_w, rank_b=e_val)
+
+        cv_tar[f'split_{j + 1}'].append(c)
+        if j == 0:
+            cv_tar['w[0]'].append(w0)
+            cv_tar['w[1]'].append(w1)
+            cv_tar['w[2]'].append(w2)
+            cv_tar['w[4]'].append(w4)
+            cv_tar['w[8]'].append(w8)
+
+cv_tar = pd.DataFrame(cv_tar)
+cv_tar['mean'] = cv_tar[[f'split_{j+1}' for j in range(n_splits)]].mean(axis=1)
+cv_tar.to_excel('model-0/cross-validation/cv_tar.xlsx')
+
+ar_max = cv_tar['mean'].argmax()
+w0 = cv_tar['w[0]'][ar_max]
+w1 = cv_tar['w[1]'][ar_max]
+w2 = cv_tar['w[2]'][ar_max]
+w4 = cv_tar['w[4]'][ar_max]
+w8 = cv_tar['w[8]'][ar_max]
+w = np.array([w0, w1, w2, 0, w4, 0, 0, 0, w8, 0])
+print(f'w = {w}')
+# w = [0.6, 0.1, -0.25, 0, 0.2, 0, 0, 0, 0.35, 0]
+
+# ----------------------------------------------------------------------
+# feature neutralization
+# ----------------------------------------------------------------------
+
+ran_nf = range(5, 55, 5)
+ran_al = [x / 100 for x in range(0, 105, 5)]
+
+cv_neu = {
+    'n_features': [],
+    'alpha': [],
+}
+for j in range(n_splits):
+    cv_neu[f'split_{j + 1}'] = []
+
+for j, (trn, val) in tqdm(enumerate(spl.split(X, y, e)), 
+                           desc='cv split', 
+                           total=n_splits,
+                           leave=None,
+                           position=0):
+    X_val = X.iloc[val]
+    y_val = Y.iloc[val]
+    e_val = e.iloc[val]
+    y_val_true = y_val[Y_TRUE]
+    y_val_pred = joblib.load(f'model-0/saved-variables/y_targets_{j}.pkl')
     y_val_pred_w = y_val_pred @ w
     
     exposures = corr(X_val, y_val_pred_w, rank_a=e_val, rank_b=e_val)
@@ -382,19 +248,24 @@ for j, (trn, val) in tqdm(enumerate(spl.split(X, y, e)),
 
         for al in tqdm(ran_al, desc='alpha', leave=False, position=2):
             y_neut = y_val_pred_w - al * y_linr
-            cr = corr(y_val_true, y_neut, rank_b=e_val)
+            c = corr(y_val_true, y_neut, rank_b=e_val)
 
-            corr_dict[f'c{j}'].append(cr)
+            cv_neu[f'split_{j + 1}'].append(c)
             if j == 0:
-                corr_dict['nf'].append(nf)
-                corr_dict['al'].append(al)
+                cv_neu['n_features'].append(nf)
+                cv_neu['alpha'].append(al)
             
 
-corr_df = pd.DataFrame(corr_dict)
-corr_df['ca'] = corr_df[['c0', 'c1', 'c2', 'c3']].mean(axis=1)
-corr_df = corr_df.sort_values('ca', ascending=False)
-corr_df.to_excel('model-0/cross-validation/cv_neu_grd.xlsx')
+cv_neu = pd.DataFrame(cv_neu)
+cv_neu['mean'] = cv_neu[[f'split_{j+1}' for j in range(n_splits)]].mean(axis=1)
+cv_neu.to_excel('model-0/cross-validation/cv_neu.xlsx')
 
+ar_max = cv_neu['mean'].argmax()
+n_features = cv_neu['n_features'][ar_max]
+alpha = cv_neu['alpha'][ar_max]
+print(f'n_features = {n_features}')
+print(f'alpha = {alpha}')
+# n_features = 30, alpha = 0.3
 
 # ======================================================================
 # final model
